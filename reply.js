fa6eda57bf5b450cd82bedea41e9aab86c364ae4
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-02-07 00:44:14 +0000

    add more reply options

--- reply.js
+++ reply.js
@@ -1,3 +1,11 @@
 export function reply(message){
-    return `${message} to you too`;
+    let reply = `${message} to you too`;
+    if(message.toLowerCase().includes("nice")){
+        reply = "Yes ... I think that's nice too";
+    }
+    
+    else if(message.toLowerCase().includes("bye")){
+        reply = "Nice talking to you, have a great rest of your day. Goodbye!"
+    }
+    return reply;
 }
